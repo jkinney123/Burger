1fbd4c13e0a5ae4a848abc1bd517614a7febac49
Joe Kinney <joekinney53@gmail.com>
2019-07-21 22:29:58 -0500

    trying to get handlebars to work

--- controllers/burgers_Controller.js
+++ controllers/burgers_Controller.js
@@ -16,32 +16,26 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/burgers", function(req, res) {
+router.post("/", function(req, res) {
   burger.create([
     "name", 
   ], [
     req.body.name
   ], function(result) {
     // Send back the ID of the new quote
-    res.json({ id: result.insertId });
+    res.redirect("/");
   });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
+router.put("/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   burger.update({
     Devoured: true
-  }, condition, function(result) {
+  }, condition, function() {
       res.redirect("/");
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
   });
 });
 
