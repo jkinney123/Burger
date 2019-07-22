7b303fd429c04e61b877af360be364820475d144
Joe Kinney <joekinney53@gmail.com>
2019-07-22 16:17:50 -0500

    edited the burger.controller.js file and fixed the routes

--- controllers/burgers_Controller.js
+++ controllers/burgers_Controller.js
@@ -16,26 +16,31 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/", function(req, res) {
+router.post("/api/burgers", function(req, res) {
   burger.create([
-    "name", 
+    "burger_name", "Devoured"
   ], [
-    req.body.name
+    req.body.burger_name, req.body.Devoured
   ], function(result) {
     // Send back the ID of the new quote
-    res.redirect("/");
+    res.json({ id: result.insertId});
   });
 });
 
-router.put("/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   burger.update({
-    Devoured: true
+    Devoured: req.body.Devoured
   }, condition, function() {
-      res.redirect("/");
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
   });
 });
 
