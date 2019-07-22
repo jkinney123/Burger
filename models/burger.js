f3e16eee24a4eb2dfdd4b48a97b9e57cf030c40c
Joe Kinney <joekinney53@gmail.com>
2019-07-22 17:06:12 -0500

    added delete function in necessary files and added in a the public burger.js

--- models/burger.js
+++ models/burger.js
@@ -17,6 +17,11 @@ var burger = {
       cb(res);
     });
   },
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
   
 };
 
--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -41,6 +41,24 @@ $(function() {
         }
       );
     });
+
+    $(".trashburger").on("click", function(event) {
+        var id = $(this).data("id");
+       
+
+    
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+          type: "DELETE",
+          url: "/api/burgers/" + id
+        }).then(
+          function() {
+        
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      });
   
     
   });
